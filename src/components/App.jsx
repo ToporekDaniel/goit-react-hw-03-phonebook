597945c44761cda4bf0d57e4dc224f863372940e
ToporekDaniel <138520262+ToporekDaniel@users.noreply.github.com>
2024-02-18 20:01:50 +0100

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,63 @@
+import { Component } from 'react';
+import { ContactList } from './list/list';
+import { Filter } from './filter/filter';
+import { ContactForm } from './form/form';
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contacts: [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ],
+      filter: '',
+    };
+  }
+
+  handleSubmit = ({ id, name, number }) => {
+    const { contacts } = this.state;
+    if (
+      contacts.some(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+    this.setState({
+      contacts: [...contacts, { id, name, number }],
+    });
+  };
+
+  handleDelete = id => {
+    const { contacts } = this.state;
+    const updatedContacts = contacts.filter(contact => contact.id !== id);
+    this.setState({
+      contacts: updatedContacts,
+    });
+  };
+
+  handleFilterChange = filterValue => {
+    this.setState({ filter: filterValue });
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter)
+    );
+    return (
+      <>
+        <h2>Phonebook</h2>
+        <ContactForm onSubmit={this.handleSubmit} />
+        <h2>Contacts</h2>
+        <Filter onFilterChange={this.handleFilterChange} />
+        <ContactList contacts={filteredContacts} onDelete={this.handleDelete} />
+      </>
+    );
+  }
+}
